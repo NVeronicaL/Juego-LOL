a35d48cacedfa86b602af400e92b71924cde5045
nveronical <verito.cba933@gmail.com>
2022-09-18 07:35:43 -0300

    Agregando metodo atacar de la clase Personaje()

--- personaje.py
+++ personaje.py
@@ -1,7 +1,21 @@
 class Personaje():
     
-    
+
     def __init__(self, id_personaje, nombre, vida):
         self.id_personaje = id_personaje
         self.nombre = nombre
         self.vida = vida
+
+
+    def atacar(self, p, ataque):
+        if p.vida > 0:
+            # for ataque in lista_attacks['ataques']:
+            #if ataque == ataque['nombre']:
+            p.vida -= ataque.puntaje
+            #print("entra", p.vida)
+                #break
+        # else:
+        #     print("***********************************************")
+        #     print("*******************GAME OVER*******************")
+        #     print("Vida del jugador", self.nombre, "en :", self.vida)
+        #     print("***********************************************")
