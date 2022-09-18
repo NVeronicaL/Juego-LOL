9640591d6048901ba24f204bfd3f8bfea04122d1
nveronical <verito.cba933@gmail.com>
2022-09-18 07:41:19 -0300

    Creación de clase ataque()

---
+++ ataque.py
@@ -0,0 +1,10 @@
+class Ataque():
+
+
+    def __init__(self, id_ataque, nombre, puntaje):
+
+        
+        self.id_ataque = id_ataque
+        self.nombre = nombre
+        self.puntaje = puntaje
+        
