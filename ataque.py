5e636f2f33fe4a5c3b263426c8279ae6ead378dd
nveronical <verito.cba933@gmail.com>
2022-09-18 22:31:55 -0300

    Implementación de metodo random_ataque en la clase Ataque()

--- ataque.py
+++ ataque.py
@@ -1,3 +1,5 @@
+import random
+
 class Ataque():
 
 
@@ -8,3 +10,12 @@ class Ataque():
         self.nombre = nombre
         self.puntaje = puntaje
         
+    def ataque_random(lista_personajes, op_personaje):
+        # Ataque aleatorio del jugador contrincante (p2)
+        for p in lista_personajes:
+            #Veo que id_personaje sea del jugador al que quiero atacar
+            if p['id_personaje'] == op_personaje:
+                random_ataque = random.choice(p['ataques'])
+                #print('random_ataque', ramdom_ataque)
+                break
+        return random_ataque
